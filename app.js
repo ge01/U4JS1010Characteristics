0743f6b725c6227c4d6471fe7d8953d95b8cb1ad
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-06-20 21:56:42 -0500

    Characteristics of JavaScript

---
+++ app.js
@@ -0,0 +1,10 @@
+/*
+    This code adds the values of
+    two variables and alerts the results.
+*/
+
+var num1 = 2; // first variable
+var num2 = 3; // second variable
+var result = num1 + num2; // calculate the sum
+// Alert the result
+alert(result);
